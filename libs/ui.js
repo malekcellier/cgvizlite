09861a3fd7aca11dcdf4939989d539401e8bafef
Malek Cellier <malek.cellier@gmail.com>
2020-03-12 00:12:20 +0100

    colorbar component

--- libs/ui.js
+++ libs/ui.js
@@ -42,7 +42,7 @@ UI.Panel = function (opts) {
     opts.id = opts.id || '';
     opts.classes = opts.classes || [];
     opts.title = opts.title || 'Panel Title';
-    opts.subtitle = opts.subtitle || 'Panel subtitle';
+    opts.subtitle = opts.subtitle || '';
     opts.closable = opts.closable || false;
 
     // The Panel is a div
@@ -81,7 +81,7 @@ UI.Panel = function (opts) {
         evt.target.parentElement.parentElement.nextElementSibling.classList.toggle('hidden');
     };
     // 1.1.2) the sublabel is OPTIONAL
-    if (opts.subtitle !== '') {
+    if (opts.subtitle !== '' || opts.subtitle) {
         let subtitle = _el({type: 'div', classes: ['subtitle']});
         subtitle.innerText = opts.subtitle;
         desc.appendChild(subtitle);
@@ -91,11 +91,11 @@ UI.Panel = function (opts) {
     let controls = _el({type: 'div', classes: ['controls', 'after']});
     head.appendChild(controls);    
     // 1.2.1) the close button is OPTIONAL
-    // TODO: should it be hidden or removed when the x is clicked?
     if (opts.closable === true) {
         let close = SvgIcon.new({icon: 'close'});
         controls.appendChild(close);
         close.onclick = (evt) => {
+            // TODO: should it be hidden or removed when the x is clicked?
             //evt.target.parentElement.parentElement.nextElementSibling.classList.toggle('hidden');
             let panel = evt.target.parentElement.parentElement.parentElement;
             panel.parentElement.removeChild(panel);
@@ -111,6 +111,93 @@ UI.Panel = function (opts) {
     return panel;
 }
 
+// Discrete ColorBar
+UI.DiscreteColorBar = function (opts) {
+    /**
+     * Discrete Colorbar
+     * 
+     * Panel with settings button for the colorbar configuration
+     * Head:
+     *  - .description:
+     *      - title: name of the represented quantity
+     *  - .controls.after
+     *      - settings: allows to set the #colors, scheme, min/max
+     * Body:
+     *  - list of divs with color and range next to it
+     */
+    // Default values
+    opts.scheme = opts.scheme || 'Spectral';
+    opts.n_colors = opts.n_colors || 7;
+    opts.reverse = opts.reverse || false;
+    opts.precision = opts.precision || 4;
+
+    let colorbar = UI.Panel(opts);
+    colorbar.classList.add('colorbar');
+    let controls = colorbar.querySelector('.controls.after');
+    let settings = SvgIcon.new({icon: 'settings'});
+    controls.appendChild(settings);
+    settings.onclick = () => {
+        console.info('clicked the settings to configure the colorbar');
+    };
+
+    // The content
+    let body = colorbar.querySelector('.panel-body');
+    body.onlick = (evt) => {
+        console.info(`clicked on ${evt.target}`);
+    };
+    // Each colorbox has its own event listener? => better to attach one to the body
+    // Get data from chroma
+    let colors = chroma.scale(opts.scheme).domain([opts.min, opts.max]).classes(opts.n_colors);
+    let values = chroma.limits([opts.min, opts.max], 'e', opts.n_colors);
+    if (opts.reverse === true) {
+        //values = chroma.limits([opts.min, opts.max], 'e', opts.n_colors);
+    }
+    for (let i=0; i<opts.n_colors; i++) {
+        // A row container for both the svg and the description
+        let container = _el({type: 'div', classes: ['colormap-item', `_${i}`]});
+        body.appendChild(container);
+
+        // 1) svg
+        let svg = SvgIcon._color_box(colors(values[i]).rgb());
+        container.appendChild(svg);
+        
+        // 2) description
+        let desc = _el({type: 'div', classes: ['colormap-item-desc']});
+        container.appendChild(desc);
+        desc.innerText = `${values[i].toPrecision(opts.precision)} to ${values[i+1].toPrecision(opts.precision)}`;
+    }
+
+    return colorbar;
+};
+
+// Header
+UI.Header = function (opts) {
+    /**
+     * Creates a header menu with options
+     * 
+     * opts: json object with fields:
+     *  - id: string
+     *  - classes: array of strings
+     *  - title: string
+     *  - subtitle: string
+     *  - closable: bool 
+     */
+    let header = this.Panel(opts);
+
+    return header;
+}
+
+// Dropdown
+
+// Slider
+
+// DoubleSlider
+
+// Switch
+
+// ColorPicker
+
+// Demo
 UI.Demo = function () {
     /**
      * Simple component to demo the ui
@@ -143,6 +230,7 @@ UI.Demo = function () {
     return demo;
 }
 
+// Icons showcase
 UI.IconsShowCase = function () {
     /**
      * Shows all the implemented icons
@@ -170,29 +258,6 @@ UI.IconsShowCase = function () {
     
     return div;
 }
-
-// Header
-UI.Header = function (opts) {
-    /**
-     * Creates a header menu with options
-     * 
-     * opts: json object with fields:
-     *  - id: string
-     *  - classes: array of strings
-     *  - title: string
-     *  - subtitle: string
-     *  - closable: bool 
-     */
-    let header = Panel(opts);
-
-    return header;
-}
-
-// Dropdown
-// Slider
-// DoubleSlider
-// Switch
-// ColorPicker
 
 // Helper functions
 
@@ -231,4 +296,19 @@ function _el(opts) {
     }
 
     return el;
+}
+
+function randRGB() {
+    let r = randInt(0, 255);
+    let g = randInt(0, 255);
+    let b = randInt(0, 255);
+
+    return [r, g, b];
+}
+
+function randInt(min, max) {
+    /**
+     * randInt: generate one value between min and max
+     */
+    return Math.round(Math.random()*(max-min) + min);
 }
