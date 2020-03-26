0dc7106ed4017637b0a48f1b060551a6115eb87e
Malek Cellier <malek.cellier@gmail.com>
2020-03-27 00:16:19 +0100

    component: resizer

--- libs/ui.js
+++ libs/ui.js
@@ -1525,7 +1525,165 @@ UI.Resizer = function (opts) {
 
 };
 
-// Color definitions
+UI.makeResizableDiv = function(div) {
+    /**
+     * Make a div resizable
+     */
+    // get the element
+    const element = document.querySelector('#' + div);
+    element.classList.add('resizable');
+    // add the resizers to it
+    element.appendChild(createResizers());
+    const resizers = document.querySelectorAll('#' + div + ' .resizer')
+    // definitions
+    const minimum_size = 20;
+    let original_width = 0;
+    let original_height = 0;
+    let original_x = 0;
+    let original_y = 0;
+    let original_mouse_x = 0;
+    let original_mouse_y = 0;
+    for (let i = 0;i < resizers.length; i++) {
+      const currentResizer = resizers[i];
+      currentResizer.addEventListener('mousedown', function(evt) {
+        evt.preventDefault();        
+        original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+        original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+        original_x = element.getBoundingClientRect().left;
+        original_y = element.getBoundingClientRect().top;
+        original_mouse_x = evt.pageX;
+        original_mouse_y = evt.pageY;
+        window.addEventListener('mousemove', resize)
+        window.addEventListener('mouseup', stopResize)
+      });
+    
+      function resize(evt) {        
+        let width;
+        let height;        
+        if (currentResizer.classList.contains('bottom-right')) {
+          width = original_width + (evt.pageX - original_mouse_x);
+          height = original_height + (evt.pageY - original_mouse_y)
+          if (width > minimum_size) {
+            element.style.width = width + 'px'
+          }
+          if (height > minimum_size) {
+            element.style.height = height + 'px'
+          }
+        }
+        else if (currentResizer.classList.contains('bottom-left')) {
+          height = original_height + (evt.pageY - original_mouse_y)
+          width = original_width - (evt.pageX - original_mouse_x)
+          if (height > minimum_size) {
+            element.style.height = height + 'px'
+          }
+          if (width > minimum_size) {
+            element.style.width = width + 'px'
+            element.style.left = original_x + (evt.pageX - original_mouse_x) + 'px'
+          }
+        }
+        else if (currentResizer.classList.contains('top-right')) {
+          width = original_width + (evt.pageX - original_mouse_x)
+          height = original_height - (evt.pageY - original_mouse_y)
+          if (width > minimum_size) {
+            element.style.width = width + 'px'
+          }
+          if (height > minimum_size) {
+            element.style.height = height + 'px'
+            element.style.top = original_y + (evt.pageY - original_mouse_y) + 'px'
+          }
+        } else if (currentResizer.classList.contains('top-middle')){
+          height = original_height - (evt.pageY - original_mouse_y)
+          if (height > minimum_size) {
+            element.style.height = height + 'px'
+            element.style.top = original_y + (evt.pageY - original_mouse_y) + 'px'
+          }
+          
+        } else if (currentResizer.classList.contains('bottom-middle')){
+          height = original_height + (evt.pageY - original_mouse_y)
+          if (height > minimum_size) {
+            element.style.height = height + 'px'
+          }
+        } else if (currentResizer.classList.contains('left-middle')){
+          width = original_width - (evt.pageX - original_mouse_x)
+          if (width > minimum_size) {
+            element.style.width = width + 'px'
+            element.style.left = original_x + (evt.pageX - original_mouse_x) + 'px'
+          }          
+        } else if (currentResizer.classList.contains('right-middle')){
+          width = original_width + (evt.pageX - original_mouse_x);
+         if (width > minimum_size) {
+            element.style.width = width + 'px'
+          }
+        }
+        else {
+          width = original_width - (evt.pageX - original_mouse_x)
+          height = original_height - (evt.pageY - original_mouse_y)
+          if (width > minimum_size) {
+            element.style.width = width + 'px'
+            element.style.left = original_x + (evt.pageX - original_mouse_x) + 'px'
+          }
+          if (height > minimum_size) {
+            element.style.height = height + 'px'
+            element.style.top = original_y + (evt.pageY - original_mouse_y) + 'px'
+          }
+        }
+      }      
+    
+      function stopResize() {
+        window.removeEventListener('mousemove', resize)
+      }
+    }
+
+    function createResizers(mode) {
+        /**
+         * creates structured dom element that will implement the edges to grab and resize a div
+         * mode: how many resizer handles
+         *  - 'one' for bottom-right
+         *  - 'middles' for all 4 middles
+         *  - 'corners', 
+         *  - 'all': 'middles' and corners
+         */
+        mode = mode || 'simple'; // options are 'one
+        let resizer = document.createElement('div');
+        resizer.classList.add('resizers');
+        
+        let positions = {
+            'simple': ['bottom-right', 'right-middle', 'bottom-middle'],
+            'middles': ['top-middle', 'bottom-middle', 'left-middle', 'right-middle'],
+            'corners': ['top-left', 'top-right', 'bottom-left', 'bottom-right']
+        };
+        
+        if (mode === 'simple') {
+            for (let i=0; i<positions[mode].length; i++) {
+                let el = document.createElement('div');
+                el.classList.add('resizer');
+                el.classList.add(positions[mode][i]);
+                resizer.appendChild(el);
+            }
+        } else {
+            if (mode === 'all' || mode === 'corners') {
+                for (let i=0; i<positions['corners'].length; i++) {
+                    let el = document.createElement('div');
+                    el.classList.add('resizer');
+                    el.classList.add(positions['corners'][i]);
+                    resizer.appendChild(el);
+                }
+            }
+            if (mode === 'all' || mode === 'middles') {
+                for (let i=0; i<positions['middles'].length; i++) {
+                    let el = document.createElement('div');
+                    el.classList.add('resizer');
+                    el.classList.add(positions['middles'][i]);
+                    resizer.appendChild(el);
+                }
+            }
+        }
+
+        return resizer;
+    }
+
+  }
+
 UI.ColorDefinitions = {
     brewer: function() {
         let brewer = {
